6d954dae88440a0b59fff50fe3f3d3977c3465fa
awetee <awedupe07@gmail.com>
2016-10-23 22:10:50 +0100

    Add collectible endpoints

---
+++ Tee.Collectibles.Api.Tests/Controllers/CollectibleControllerTest.cs
@@ -0,0 +1,102 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using FluentAssertions;
+using NSubstitute;
+using NUnit.Framework;
+using Tee.Collectibles.Api.Controllers;
+using Tee.Collectibles.Api.IServices;
+using Tee.Collectibles.Common.Entities;
+
+namespace Tee.Collectibles.Api.Tests.Controllers
+{
+    [TestFixture]
+    public class CollectibleControllerTest
+    {
+        private ICollectibleService collectableService;
+        private CollectibleController controller;
+        private List<Collectible> collectibles;
+
+        [SetUp]
+        public void Setup()
+        {
+            this.collectableService = Substitute.For<ICollectibleService>();
+            this.controller = new CollectibleController(this.collectableService);
+            this.collectibles = new List<Collectible>
+            {
+                new Collectible {Id = 1, Title = "First"},
+                new Collectible {Id = 2, Title = "Second"},
+            };
+        }
+
+        [Test]
+        public void Get_Returns_AListOfCollectibles_GivenNoParameter()
+        {
+            // Arrange
+
+            this.collectableService.GetAll().Returns(collectibles.AsEnumerable());
+
+            // Act
+            var result = controller.Get().ToList();
+
+            // Assert
+            result.Should().NotBeNull();
+            result.Count().ShouldBeEquivalentTo(2);
+        }
+
+        [Test]
+        public void Get_Returns_Collectible_GivenAnId()
+        {
+            // Arrange
+            var id = 1;
+            this.collectableService.Get(id).Returns(collectibles.First(c => c.Id == id));
+
+            // Act
+            var result = controller.Get(id);
+
+            // Assert
+            result.Should().NotBeNull();
+            result.Id.ShouldBeEquivalentTo(id);
+        }
+
+        [Test]
+        public void Post()
+        {
+            // Arrange
+            var collectible = new Collectible();
+            this.collectableService.Add(collectible).Returns(3);
+
+            // Act
+            var result = controller.Post(collectible);
+
+            // Assert
+            result.ShouldBeEquivalentTo(3);
+        }
+
+        [Test]
+        public void Put()
+        {
+            // Arrange
+            var collectible = new Collectible();
+
+            // Act
+            controller.Put(collectible);
+
+            // Assert
+            this.collectableService.Received(1).Update(collectible);
+        }
+
+        [Test]
+        public void Delete()
+        {
+            // Arrange
+            var id = 1;
+            this.collectableService.Get(1).Returns(this.collectibles.First(c => c.Id == id));
+
+            // Act
+            controller.Delete(1);
+
+            // Assert
+            this.collectableService.Received(1).Delete(this.collectibles.First(c => c.Id == id));
+        }
+    }
+}
---
+++ Tee.Collectibles.Api/Controllers/CollectibleController.cs
@@ -0,0 +1,46 @@
+﻿using Tee.Collectibles.Common.Entities;
+
+namespace Tee.Collectibles.Api.Controllers
+{
+    using System.Collections.Generic;
+    using System.Web.Http;
+    using Tee.Collectibles.Api.IServices;
+
+    [Authorize]
+    public class CollectibleController : ApiController
+    {
+        private readonly ICollectibleService _collectibleService;
+
+        public CollectibleController(ICollectibleService collectibleService)
+        {
+            _collectibleService = collectibleService;
+        }
+
+        public IEnumerable<Collectible> Get()
+        {
+            return this._collectibleService.GetAll();
+        }
+
+        [Route("GetById/{id}")]
+        public Collectible Get(int id)
+        {
+            return this._collectibleService.Get(id);
+        }
+
+        public int Post(Collectible data)
+        {
+            return this._collectibleService.Add(data);
+        }
+
+        public void Put(Collectible data)
+        {
+            this._collectibleService.Update(data);
+        }
+
+        public void Delete(int id)
+        {
+            var collectible = this._collectibleService.Get(id);
+            this._collectibleService.Delete(collectible);
+        }
+    }
+}
